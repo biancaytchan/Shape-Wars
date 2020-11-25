6a653438e5c7f9f7c06a0497cd49e717e846b667
AthenaBC <biancaytchan@gmail.com>
2020-11-25 01:03:43 +0000

    init game set up

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-canvas.width = 600;
-canvas.height = 600;
+var canvas = document.getElementById("gameCanvas")
+var ctx = canvas.getContext("2d")
+
+var main = function(){
+  window.requestAnimationFrame(main);
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+}
+
+function startGame(){
+main()
+}
+
+startGame()
